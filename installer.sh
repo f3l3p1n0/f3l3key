e6127d7707d1422707dc50757cc898fb98c25fd0
f3l3p1n0 <marc.39111@hotmail.com>
2023-05-17 14:24:57 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -15,7 +15,7 @@ function install_shc {
 
 function tarea_cron {
     sudo crontab -e
-    echo -e "$(crontab -l)\n* * * * * bash ~/Descargas/f3l3key/tester.sh" | crontab -
+    echo -e "$(crontab -l)\n* * * * * bash /home/user/Descargas/f3l3key/tester.sh" | crontab -
     sudo service cron start
 }
 
